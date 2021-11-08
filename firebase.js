c86bd44632c497e03a95689fdeea7990f6a42069
oculi-s <oculis0925@yonsei.ac.kr>
2021-11-08 10:47:02 +0900

    211108

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAuuLVy94PUS8YtEfhibbtHewCsrImhhfM",
@@ -15,10 +15,14 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 const db = getFirestore();
 const $ = document.querySelector.bind(document);
+const de = decodeURI;
+const en = encodeURI;
 
-var url = decodeURI(window.location.href).replace('.html', '').split('//')[1].split('/').slice(1);
+var url = de(window.location.href).replace('.html', '').split('//')[1].split('/').slice(1);
 if (url[0] == '')
   url = ['futures']
+if (url[1] == '')
+  url[1] = 'index'
 if (url[1] == 'index')
   url.push('index')
 if (url[2] == '')
@@ -30,19 +34,30 @@ var style = document.createElement('style');
 style.innerHTML = css.data()['github_markdown'];
 $('head').appendChild(style);
 
-var html = doc(db, url[0], url[1]);
-var html = await getDoc(html);
-var html = decodeURI(html.data()[url[2]].replace('\n','').replace('\t','').replace(' ',''))
-$('body').innerHTML = html;
+async function getData() {
+  var html = doc(db, url[0], url[1]);
+  var html = await getDoc(html);
+  var html = de(html.data()[url[2]].replace('%0A;', ''));
+  return html;
+}
+getData().then((html) => { $('body').innerHTML = html });
 
-function edit(){
+function edit() {
   $('section').innerHTML = '<textarea>';
-  $('textarea').innerHTML = html;
+  getData().then((html) => { $('textarea').value = html });
   $('textarea').style = "width:100%; height:100%;"
 }
 
-function save(){
-  var dict = {}
-  dict[url[2]] = encodeURI($('textarea').innerHTML);
-  // await setDoc(doc(db, url[0], url[1]), dict);
+async function save() {
+  var dict = {};
+  dict[url[2]] = en($('textarea').value);
+  dict[url[2]] = dict[url[2]].replaceAll('%0A', '');
+  while (dict[url[2]].includes('%20%20')) {
+    dict[url[2]] = dict[url[2]].replaceAll('%20%20', '%20');
+  }
+  await updateDoc(doc(db, url[0], url[1]), dict);
+  await getData().then((html) => { $('body').innerHTML = html });
 }
+window.getData = getData;
+window.edit = edit;
+window.save = save;
