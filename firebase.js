6dfc64e9a6cd9d4a3303dbfb09b592e013971126
oculi-s <kjunhun9@sogang.ac.kr>
2021-11-25 19:01:27 +0900

    211125

--- firebase.js
+++ firebase.js
@@ -24,6 +24,7 @@ const en = encodeURI;
 
 $('head').innerHTML += `<meta name="viewport" content="width=device-width, initial-scale=1.0"/>`;
 $('head').innerHTML += `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">`
+$('head').innerHTML += `<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css">`
 var url = de(window.location.href).split('//')[1].split('/').slice(1);
 if (url[0] == 'blog')
     url = url.slice(1);
