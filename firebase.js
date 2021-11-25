713baf7d2ecd8a73b648ac90f45dd8630c3f70bf
unknown <kjunhun9@sogang.ac.kr>
2021-11-25 22:48:11 +0900

    211125

--- firebase.js
+++ firebase.js
@@ -113,7 +113,7 @@ getWidget().then(async() => {
     var editsave = await getDoc(doc(db, 'source', 'editsave'));
     $('section').innerHTML += de(editsave.data().index[user.data().auth]);
     setScript(setData(html));
-    $('head').innerHTML += `<script type='text/javascript' src='/blog/prettify.js'></script>`;
+    $('head').innerHTML += `<script src='https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=desert'></script>`;
     // var prettify = await getDoc(doc(db, 'source', 'prettify'));
     // eval(prettify.data().index[true]);
     // window.prettify = prettify.data().index[true];
